ff89a512471bbaea2261ee81516a23468ade20dc
saimadhu2017 <madhusai2017@gmail.com>
2022-09-04 23:31:27 +0530

    Fixed the bug and done the file formating

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -36,7 +36,7 @@ const ItemPreview = (props) => {
     >
       <img
         alt="item"
-        src={item.image || require('../imgs/placeholder.png')}
+        src={item.image || require("../imgs/placeholder.png")}
         className="card-img-top item-img"
         style={{ borderRadius: "20px" }}
       />
